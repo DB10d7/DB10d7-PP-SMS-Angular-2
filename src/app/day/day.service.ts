3aa704e3a2c6d2b93fd80fb54f32bc72e7659e65
DB10d7 <banerjeedebdeep0@gmail.com>
2021-11-01 14:31:08 +0530

    batch-day-student

--- src/app/day/day.service.ts
+++ src/app/day/day.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { AddStudentToDayRequest } from './add-student-to-day/addStudentToDay.request.payload';
 
 @Injectable({
   providedIn: 'root'
@@ -16,9 +17,9 @@ export class DayService {
     console.log(this.httpClient.get('http://localhost:8080/api/days/by-batch/'+ name));
     return this.httpClient.get<any>('http://localhost:8080/api/days/by-batch/'+ name);
   }
-  addStudentToDay(res: any): Observable<any>{
-    console.log(this.httpClient.post('http://localhost:8080/api/days/addStudent/'+ res));
-    return this.httpClient.post('http://localhost:8080/api/days/addStudent/'+ res);
+  addStudentToDay(addStudentToDayRequest: AddStudentToDayRequest ): Observable<any>{
+    console.log(this.httpClient.put('http://localhost:8080/api/days/addStudent/', addStudentToDayRequest, { responseType: 'text' } ));
+    return this.httpClient.put('http://localhost:8080/api/days/addStudent/', addStudentToDayRequest, { responseType: 'text' } );
   } 
   viewDay(name: String):Observable<any>{
     console.log(this.httpClient.get('http://localhost:8080/api/days/'+name));
