c30e1c1a3339bcf2aeac887306c1461900430605
DB10d7 <banerjeedebdeep0@gmail.com>
2021-12-10 17:48:54 +0530

    mail-image

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -125,7 +125,7 @@ export class SignupComponent implements OnInit {
           this.uploadUserImage();
         }else{
           alert(data);
-          this.ngOnInit();
+          
         }
         
       }, error => {
