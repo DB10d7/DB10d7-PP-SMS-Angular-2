ba72171b4a105c61adbeb550f88c2d3ead2c04b9
Debdeep Banerjee <banerjeedebdeep0@gmail.com>
2022-02-08 18:01:10 +0530

    student-page

--- src/app/student/single-student/single-student.component.ts
+++ src/app/student/single-student/single-student.component.ts
@@ -17,6 +17,14 @@ export class SingleStudentComponent implements OnInit {
   retrievedImage: any;
   base64Data: any;
   retrieveResonse: any;
+  setDay: Set<string>= new Set();
+  jobsAppliedList: any;
+  numberOfJobs: number=0;
+  studentApi: any;
+  practicePercent: number=0;
+  totalCompletion: number=0;
+  practiceTopics: any;
+  practiceTrack!:any
   constructor(private studentService:StudentService, private authService : AuthService,public dayService: DayService,private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
@@ -36,6 +44,9 @@ export class SingleStudentComponent implements OnInit {
       console.log(this.singleStudent.username);
       console.log("data is here",result);
       this.listDayByStudent= result;
+      for(var i=0;i<this.listDayByStudent.length;i++){
+        this.setDay.add(this.listDayByStudent[i].dayName);
+      }
       console.log(this.listDayByStudent);
       this.viewDayListByBatch();
     })
@@ -47,8 +58,15 @@ export class SingleStudentComponent implements OnInit {
       this.listDayByBatch= result;
       console.log(this.listDayByBatch);
       console.log(this.listDayByStudent.length)
-      this.userAttendance = ((this.listDayByStudent.length *100)/this.listDayByBatch.length);
+      if(this.listDayByStudent.length != 0){
+        this.userAttendance = ((this.listDayByStudent.length *100)/this.listDayByBatch.length);
+      }
+      
+      // if(this.userAttendance === NaN){
+      //   this.userAttendance = 0
+      // }
       console.log(this.userAttendance);
+      this.viewJobDetails();
     })
   }
   getImage() {
@@ -62,5 +80,57 @@ export class SingleStudentComponent implements OnInit {
           console.log(this.retrieveResonse);
         }
       );
+  }
+  viewJobDetails(){
+    this.studentService.viewJobListByStudent(this.singleStudent.email).subscribe((result)=>{
+      var list=result;
+      console.log(list);
+      this.jobsAppliedList=list.myjobs;
+      console.log(this.jobsAppliedList);
+      if(this.jobsAppliedList.length){
+        this.numberOfJobs = this.jobsAppliedList.length;
+      }
+      this.viewPracticeDetails();
+    },(err)=>{
+      console.log(err);
+      this.viewPracticeDetails();
+    })
+  }
+  viewPracticeDetails(){
+    var batch = this.singleStudent.batch;
+    console.log(batch);
+    console.log(batch.length);
+    var search='';
+    for(var i=0;i<batch.length;i++){
+      
+      if(batch[i]>='A' && batch[i]<='Z'){
+        search+=batch[i].toLowerCase();
+        console.log(batch[i]);
+      }else if(batch[i]>='0' && batch[i]<='9'){
+        search+=batch[i];
+        console.log(batch[i]);
+      }
+    }
+    console.log(search);
+    this.studentService.viewPracticeByStudent(search).subscribe((result)=>{
+      var list = result;
+      console.log(list);
+      for(var i=0;i<list.length;i++){
+        if((''+list[i].id) === this.singleStudent.username){
+          var student = list[i];
+          console.log(student.practice_percent.percentage);
+          if(student.practice_percent){
+            this.practicePercent = student.practice_percent.percentage;
+          }
+          this.totalCompletion = (this.practicePercent + this.userAttendance)/2;
+          console.log(this.practicePercent);
+          console.log(student);
+          this.practiceTrack = student.practice_track;
+          console.log(this.practiceTrack);
+          this.practiceTopics = Object.keys;
+          // console.log(this.practiceTopics[4]);
+        }
+      }
+    })
   }
 }
--- src/app/student/student.service.ts
+++ src/app/student/student.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpContext } from '@angular/common/http';
 import { environment } from 'src/environments/environment.prod';
+import { BYPASS_LOG } from 'src/app/token-interceptor';
 // import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -10,6 +11,8 @@ import { environment } from 'src/environments/environment.prod';
 export class StudentService {
 
   host:string= environment.apiUrl;
+  xploreJobsApi:string='https://xplore.co.in/apiuser?token=ppofficerwx4356&mode=job&email=';
+  ppPracticeApi: string='https://learn.packetprep.com/apiuser?token=ppofficerwx4356&info=b25&practice=java-programming';
   constructor(private httpClient: HttpClient) { }
   getStudentList(): Observable<any>{
     console.log(this.httpClient.get(this.host+'api/students/'));
@@ -26,6 +29,14 @@ export class StudentService {
   getStudentListByDay(name: String): Observable<any>{
     console.log(this.httpClient.get(this.host+'api/students/by-day/'+ name));
     return this.httpClient.get<any>(this.host+'api/students/by-day/'+ name);
+  }
+  viewJobListByStudent(email: String):Observable<any>{
+    console.log(this.httpClient.get(this.xploreJobsApi+email,{ context: new HttpContext().set(BYPASS_LOG, true) }));
+    return this.httpClient.get<any>(this.xploreJobsApi+email,{ context: new HttpContext().set(BYPASS_LOG, true) });
+  }
+  viewPracticeByStudent(batch: String):Observable<any>{
+    console.log(this.httpClient.get('https://learn.packetprep.com/apiuser?token=ppofficerwx4356&info='+ batch + '&practice=java-programming',{ context: new HttpContext().set(BYPASS_LOG, true) }));
+    return this.httpClient.get<any>('https://learn.packetprep.com/apiuser?token=ppofficerwx4356&info='+ batch + '&practice=java-programming',{ context: new HttpContext().set(BYPASS_LOG, true) });
   }
   
 }
